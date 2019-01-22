21c388a28468a06c19318a1ed30894e48ec45b1d
farisaliyafie <faris.ali@nostratech.com>
2019-01-22 13:43:12 +0700

    First Commit

---
+++ src/main/java/cobacoba/Test/NostraApplication.java
@@ -0,0 +1,14 @@
+package cobacoba.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NostraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NostraApplication.class, args);
+	}
+
+}
+
